5952da8a3a72642c0bc60f84afda595c2c7214d1
xiangsheng1325 <xiangsheng1325@sjtu.edu.cn>
2021-02-23 10:41:38 +0800

    update preprocessing utils.py

---
+++ GraphGenerator/preprocessing/utils.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import sys
+
+
+def edgelist_to_graph(path):
+    try:
+        graph = nx.read_edgelist(path)
+        return graph
+    except:
+        print("Wrong path entered! Absolute path of edgelist file pxpected.")
+        sys.exit(0)
+
+
+def pathlist_to_graphlist(path):
+    with open(path, "r") as f:
+        path_list = f.readlines()
+    path_list = [p.strip("\n") for p in path_list if p != "\n"]
+    graph_list = [edgelist_to_graph(p) for p in path_list]
+    return graph_list
