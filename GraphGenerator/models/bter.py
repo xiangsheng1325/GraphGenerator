449124e8d065a2775bdd641af4d809b62ccd2da8
xiangsheng1325 <xiangsheng1325@sjtu.edu.cn>
2021-03-30 14:23:18 +0800

    add bter configuration file

--- GraphGenerator/models/bter.py
+++ GraphGenerator/models/bter.py
@@ -2,7 +2,7 @@ import os
 from GraphGenerator.utils.data_utils import load_matlab_graph, save_matlab_graph
 
 
-def generate_matlab_script(data_name, in_mat_path, out_mat_path):
+def generate_matlab_mat(data_name, in_mat_path, out_mat_path):
     bter_path = "./GraphGenerator/models/bter_ops/"
     template_filename = os.path.join(bter_path, "template.m")
     with open(template_filename, "r") as r_f:
@@ -15,15 +15,16 @@ def generate_matlab_script(data_name, in_mat_path, out_mat_path):
         w_f.write(context[1])
         w_f.write(os.path.join("../../../", out_mat_path))
         w_f.write(context[2])
+    os.system("matlab 'cd ./GraphGenerator/models/bter_ops; {}; cd ../../..; quit'".format(data_name))
+    os.remove(tmp_filepath)
 
 
 def bter(input_graph, config):
-    fname = os.path.join(config.exp_dir, config.exp_name, "{}.mat".format(config.data.name))
-    dump_name = os.path.join(config.exp_dir, config.exp_name, "bter_to_{}.mat".format(config.data.name))
-    save_matlab_graph(fname, input_graph, config.data.name)
-    generate_matlab_script(data_name=config.data.name,
-                           in_mat_path=fname,
-                           out_mat_path=dump_name)
-    os.system("matlab 'cd ./GraphGenerator/models/bter_ops; {}; cd ../../..; quit'".format(config.data.name))
+    fname = os.path.join(config.exp_dir, config.exp_name, "{}.mat".format(config.dataset.name))
+    dump_name = os.path.join(config.exp_dir, config.exp_name, "bter_to_{}.mat".format(config.dataset.name))
+    save_matlab_graph(fname, input_graph, config.dataset.name)
+    generate_matlab_mat(data_name=config.dataset.name,
+                        in_mat_path=fname,
+                        out_mat_path=dump_name)
     G, _ = load_matlab_graph(fname=dump_name)
     return G
